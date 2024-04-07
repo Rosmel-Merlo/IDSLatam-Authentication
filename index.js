0fac8c39d25f8703f674e28d8074dad15653a0ba
Rosmel Merlo <rosmel2508@outlook.com.pe>
2024-04-06 23:39:40 -0500

    Actualización del index

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-const quotes = [
-  "Este",
-  "es mi primera versión",
-  "proyecto npm",
-  "se va actualizar con el contenido adecuado"
-];
+const keyJwt = "Jwt";
+const keyAccess = "Access";
+const CLAIM_EXP = "exp";
 
-/**
- * Gets a random Piñera Quote
- * @returns {string}
- */
-function randomQuote() {
-  return quotes[Math.floor(Math.random() * quotes.length)];
-}
+const getAccessLocalStorage = () => {
+  let c = localStorage.getItem(keyAccess);
+  if (c) {
+    return JSON.parse(c);
+  }
 
+  return null;
+};
+
+const getToken = () => {
+  let c = localStorage.getItem(keyJwt);
+  if (c && c != "") {
+    return c.substring(1, c.length - 1);
+  }
+  return null;
+};
+
+const getInfoToken = (key) => {
+  let s = getToken();
+
+  if (s) {
+    let t = JSON.parse(atob(s.split(".")[1]));
+    return t[key];
+  }
+
+  return null;
+};
+
+const removeToken = () => {
+  localStorage.removeItem(keyJwt);
+};
+
+const expiroToken = () => {
+  let exp = new Date(getInfoToken(CLAIM_EXP) * 1000);
+  let now = new Date();
+
+  if (exp < now) {
+    return true;
+  }
+  return false;
+};
+
+const cambiarUrl = (url) => {
+  let urlAux = url?.replace("//", "@");
+  return urlAux?.replace(/\//g, "!");
+};
+
+const RedireccionUrlIdentidad = (urlBase, email, token, urlLogin) => {
+  const urlRedireccin = `${urlBase}/cambiarContrasenia?email=${email}&token=${token}&urlLogin=${urlLogin}`;
+  return urlRedireccin;
+};
 module.exports = {
-  randomQuote
+  getAccessLocalStorage,
+  getToken,
+  getInfoToken,
+  expiroToken,
+  removeToken,
+  cambiarUrl,
+  RedireccionUrlIdentidad,
 };
